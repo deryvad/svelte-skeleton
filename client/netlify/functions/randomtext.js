498a5d84e1ffbfc471940982a22750c6c8539404
deryvad <davyred013@gmail.com>
2022-07-25 14:01:29 +0800

    added lorem ipsum

--- client/netlify/functions/randomtext.js
+++ client/netlify/functions/randomtext.js
@@ -1,6 +1,22 @@
+const LoremIpsum = require("lorem-ipsum").LoremIpsum;
+
+const lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+        max: 8,
+        min: 4
+    },
+    wordsPerSentence: {
+        max: 16,
+        min: 4
+    }
+});
+
+
 exports.handler = async function (event, context) {
+  
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Hello World" }),
+      body: lorem.generateSentences(parseInt(200)),
     };
   }
